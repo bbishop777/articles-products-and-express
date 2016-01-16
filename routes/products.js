498e10509170cffe7a0d8f4d0db212a3decc9b15
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-01-16 09:25:49 -1000

    pushing objects to db route with an id, also changed request.body.price into a number. created a idCounter

--- routes/products.js
+++ routes/products.js
@@ -2,6 +2,7 @@ var express = require('express');
 var fs = require('fs');
 var router = express.Router();
 var productInventory = require('./../db/products.js');
+var idCounter = 0;
 
 router.get('/', function(request, response){
   response.send('aloha');
@@ -11,21 +12,28 @@ router.get('/', function(request, response){
 router.post('/', function(request, response){
   var postRequestValidation = ['name', 'price', 'inventory'];
 
-  console.log(request.body);
+  // console.log(request.body);
+  // console.log(request.body.hasOwnProperty('name'));
 
-  //console.log(request.body.hasOwnProperty(name));
+  for(var i = 0; i < postRequestValidation.length ; i++){
+    if(!request.body.hasOwnProperty(postRequestValidation[i])){
+      return response.send(false + ' needs to have name, price, and inventory keys');
+    }
 
-  // for(var i = 0; i < postRequestValidation.length ; i++){
-  //   if(!request.body.hasOwnProperty(postRequestValidation[i])){
-  //     return response.send(false + ' needs to have name, price, and inventory keys');
-  //   }
+    if(request.body[postRequestValidation[i] ].length === 0){
+      return response.send(false + " missing " + postRequestValidation + 'value');
+    }
+  }
 
-  //   if(request.body[postRequestValidation[i] ].length === 0){
-  //     return response.send(false + " missing " + postRequestValidation + 'value');
-  //   }
-  // }
+  var productObject = { 'name': request.body.name ,
+                        'price' : parseInt(request.body.price) ,
+                        'inventory': request.body.inventory ,
+                        'id' : idCounter
+                        };
 
-  productInventory.push(request.body);
+  idCounter++;
+
+  productInventory.push(productObject);
   console.log(productInventory);
   response.end();
 });
--- server.js
+++ server.js
@@ -22,5 +22,5 @@ var server = app.listen(3000, function(){
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('connected');
+  console.log('Connected to', port);
 });
